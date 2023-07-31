ca92c3ba0ce79d261dc3c0d2e8c08b737c5c0235
olpasvd <141055846+olpasvd@users.noreply.github.com>
2023-07-31 18:13:28 +0200

    Create README.m
    v

---
+++ README.m
@@ -0,0 +1,17 @@
+--BROUGHT TO YOU BY ROBLOXSCRIPTS.NET!--
+
+-- # BETA V3 # --  
+
+collectAllFruit = true --[ TRUE / FALSE ]-- 
+collectOnlyFruitSpawn = false --[ TRUE / FALSE ]--
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/BetaFruitFinder"))()- 👋 Hi, I’m @olpasvd
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+olpasvd/olpasvd is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
